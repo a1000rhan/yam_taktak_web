1706cb42cb7979e4267169d2ccf56bfe6e0dd8a5
a1000rhan <1000rhan@gmail.com>
2024-10-26 22:21:57 +0300

    .

--- src/api/API.jsx
+++ src/api/API.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "http://localhost:8000/api",
+  baseURL: "http://localhost:3030/api",
   // baseURL: "https://e64a-178-61-142-203.ngrok-free.app/api",
-  baseURL: "https://api.ma3lomatkw.com/api",
+  // baseURL: "https://api.ma3lomatkw.com/api",
 });
 
 export default API;
