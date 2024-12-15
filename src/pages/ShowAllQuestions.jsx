d5d09b389ca01c90324a5a553bdb9d160d51300b
a1000rhan <1000rhan@gmail.com>
2024-12-15 20:46:07 +0300

    Add video support for questions and answers in ShowAllQuestions component

--- src/pages/ShowAllQuestions.jsx
+++ src/pages/ShowAllQuestions.jsx
@@ -17,31 +17,52 @@ const ShowAllQuestions = () => {
     fetchQuestions();
   }, []);
 
-  const listAllQuestions = questions.map((question) => (
-    <div>
-      <tr key={question._id}>
-        <td>{question.question}</td>
-        <td>{question.answer}</td>
-        <td>{question.category.categoryName}</td>
-        <td>{question.weight}</td>
+  const listAllQuestions = questions.map((question) => {
+    const isVideoQuestion =
+      question.questionImage.toLowerCase().includes("mov") ||
+      question.questionImage.toLowerCase().includes("mp4");
+    const isVideoAnswer =
+      question.answerImage.toLowerCase().includes("mov") ||
+      question.answerImage.toLowerCase().includes("mp4");
 
-        <td>
-          <img
-            src={question.questionImage}
-            alt="question._id"
-            className="thumbnail-img"
-          />
-        </td>
-        <td>
-          <img
-            src={question.answerImage}
-            alt="question._id"
-            className="thumbnail-img"
-          />
-        </td>
-      </tr>
-    </div>
-  ));
+    return (
+      <div>
+        <tr key={question._id}>
+          <td>{question.question}</td>
+          <td>{question.answer}</td>
+          <td>{question.category.categoryName}</td>
+          <td>{question.weight}</td>
+
+          <td>
+            {isVideoQuestion ? (
+              <video width="320" height="240" controls>
+                <source src={question.questionImage} type="video/mp4" />
+              </video>
+            ) : (
+              <img
+                src={question.questionImage}
+                alt={question._id}
+                className="thumbnail-img"
+              />
+            )}
+          </td>
+          <td>
+            {isVideoAnswer ? (
+              <video width="320" height="240" controls>
+                <source src={question.answerImage} type="video/mp4" />
+              </video>
+            ) : (
+              <img
+                src={question.answerImage}
+                alt={question._id}
+                className="thumbnail-img"
+              />
+            )}
+          </td>
+        </tr>
+      </div>
+    );
+  });
 
   return (
     <>
