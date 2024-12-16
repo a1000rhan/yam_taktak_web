6f82068e0f958346d8d7e18bf59e0f985730db60
A.AAlFarhan <a.aalfarhan@gulfbank.com.kw>
2024-12-16 22:00:08 +0300

    Add environment configuration files and update API URL handling

--- src/api/API.jsx
+++ src/api/API.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
-
+const apiUrl = process.env.REACT_APP_API_URL;
 const API = axios.create({
   // baseURL: "http://localhost:3030/api",
   // baseURL: "https://e64a-178-61-142-203.ngrok-free.app/api",
-  baseURL: "https://ma3lomatkw.com/api",
+  baseURL: `${apiUrl}/api`,
 });
 
 export default API;
